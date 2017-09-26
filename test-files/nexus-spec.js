d7ac7e602acd4b5e2480e669f518e3dc3bf69226
Jason Steck <Jason.Steck+JSNexus@gmail.com>
2017-09-25 18:45:59 -0600

    Minor var extract

--- test-files/nexus-spec.js
+++ test-files/nexus-spec.js
@@ -1,10 +1,11 @@
+const ServerAddr = 'ws://localhost:3000';
 describe('JS-Nexus', function() {
   // Throw error if a Nexus server is not running
   ensureNexusServerIsRunning();
 });
 
 function ensureNexusServerIsRunning() {
-  const ws = new WebSocket('ws://localhost:3000');
+  const ws = new WebSocket(ServerAddr);
   ws.onclose = function() {
     console.error("No Connection to Nexus Server!");
   }
